a68548ff46c6e18f35b1cd688714137f3210e25e
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2023-05-11 23:36:31 -0600

    Mejoras de extracción
    - Se reajustaron las consultas de registros para registrar los datos.
    - Se añadieron las fases respectivas a sus ingestas.
    - Se añadió una segunda conexión a Postgres para ingestar los clientes.
    - Se fusionaron las extracciones de datos demográficos y datos geográficos en una sola extracción a clientes.

--- extraccion.py
+++ extraccion.py
@@ -1,12 +1,22 @@
 from sqlalchemy.engine.base import Connection
 from sqlalchemy import text
 from typing import Any, Dict
-from datetime import datetime, date, timedelta
+from datetime import datetime, date
 import oracledb
 import psycopg2
 import pandas as pd
 import sqlalchemy
 import calendar
+
+
+class MemoryLogger:
+    __log: str
+
+    def log(self, message: str) -> None:
+        self.__log = self.__log + message + '\n'
+
+    def get_log(self) -> str:
+        return self.__log
 
 
 def get_oracle_conn() -> oracledb.Connection:
@@ -35,10 +45,81 @@ def get_postgres_conn() -> psycopg2.Connection:
         host="34.72.193.129",
         user="postgres",
         password="%Xh@q/_-<V\"~1.8.",
+        database="postgres",
+        options="-c search_path=public",
     )
 
 
-def extract_dataset(origin: Connection, destination: Connection, query: str, table: str, params: Dict[str, Any] = None, limit: int = None):
+def get_masters_conn() -> psycopg2.Connection:
+    return psycopg2.connect(
+        host="34.72.193.129",
+        user="alexo",
+        password="Alexo123",
+        database="postgres",
+        options="-c search_path=maestros",
+    )
+
+
+def count_rows(conn: Connection, table: str) -> int:
+    return conn.scalar(text(f"""
+    SELECT COUNT(*) AS count
+    FROM {table}
+    """))
+
+
+def write_binnacle(conn: Connection, term: int, phase: int, start: datetime, end: datetime) -> None:
+    cursor = conn.execute(text("""
+    SELECT amarillo, rojo
+    FROM tcgespro_fase
+    WHERE id_fase = :phase
+    """), {'phase': phase})
+
+    if cursor.rowcount != 1:
+        print(f'Phase {phase} not found')
+        return
+
+    delta = end - start
+    service_level = cursor.fetchone()
+
+    flag: str
+    if delta <= service_level[0]:
+        flag = "Verde"
+    elif delta <= service_level[1]:
+        flag = "Amarillo"
+    else:
+        flag = "Rojo"
+
+    conn.execute(text("""
+    INSERT INTO ttgespro_bitacora_estado_cuenta (fecha_hora_inicio, fecha_hora_final, bandera_nivel_servicio, id_formato_estado_cuenta, id_periodo, id_fase) 
+    VALUES (:start, :end, :flag, :format, :term, :phase)
+    """), {
+        "start": start,
+        "end": end,
+        "flag": flag,
+        "format": 0,
+        "term": term,
+        "phase": phase,
+    })
+
+
+def extract_simple_indicator(conn: Connection, table: str, column: str) -> float:
+    return round(conn.scalar(text(f"""
+    SELECT SUM({column})
+    FROM {table}
+    """)), 2)
+
+
+def extract_dataset(
+    origin: Connection,
+    destination: Connection,
+    query: str,
+    table: str,
+    term: int,
+    phase: int,
+    params: Dict[str, Any] = None,
+    indicator: str = None,
+    limit: int = None,
+):
     if params is None:
         params = {}
     if limit is not None:
@@ -47,8 +128,6 @@ def extract_dataset(origin: Connection, destination: Connection, query: str, tab
 
     print(f"Extracting {table}...")
 
-    service_level = timedelta(hours=2)
-
     start = datetime.now()
     df_pd = pd.read_sql_query(text(query), origin, params=params)
     df_pd.to_sql(table, destination, if_exists="replace")
@@ -56,31 +135,20 @@ def extract_dataset(origin: Connection, destination: Connection, query: str, tab
     df_sp = spark.createDataFrame(df_pd)
     df_sp.write.parquet(f"gs://spark-dataproc-poc/Datalake/{table}", mode="overwrite")
     end = datetime.now()
-    delta = end - start
 
-    flag: str
-    if delta <= service_level:
-        flag = "Verde"
-    elif delta <= service_level + timedelta(hours=1):
-        flag = "Amarillo"
-    else:
-        flag = "Rojo"
+    rows = count_rows(destination, table)
+    app_log.log(f"Extracted {rows} rows from '{table}'")
 
-    destination.execute(text("""
-    INSERT INTO tcgespro_fase (descripcion, descripcion_corta, nivel_servicio, bandera, inicio_periodo, fin_periodo) 
-    VALUES (:descripcion, :descripcion_corta, :nivel_servicio, :bandera, :inicio_periodo, :fin_periodo)
-    """), {
-        "descripcion": f"Extracción de tabla '{table}' desde Oracle a Postgres y Parquet",
-        "descripcion_corta": f"Extracción de tabla '{table}'",
-        "nivel_servicio": service_level.total_seconds(),
-        "bandera": flag,
-        "inicio_periodo": start,
-        "fin_periodo": end,
-    })
+    if indicator is not None:
+        statistics = extract_simple_indicator(destination, table, indicator)
+        app_log.log(f"Indicator {indicator} have a value of {statistics} from '{table}'")
+
+    write_binnacle(destination, term, phase, start, end)
 
     print(f"Done extracting {table}!")
 
 
+app_log = MemoryLogger()
 oracledb.init_oracle_client()
 
 oracle_pool = sqlalchemy.create_engine(
@@ -107,6 +175,14 @@ with postgres_pool.connect() as conn:
     conn.execute(text("SELECT 'OK'"))
     print("Postgres connection OK")
 
+masters_pool = sqlalchemy.create_engine(
+    "postgresql+psycopg2://",
+    creator=get_masters_conn,
+)
+with masters_pool.connect() as conn:
+    conn.execute(text("SELECT 'OK'"))
+    print("Postgres connection OK")
+
 print("Year period")
 year = int(input())
 print("Month period")
@@ -121,61 +197,67 @@ print(f"Extracting period: from {start_month} to {end_month}")
 with postgres_pool.begin() as postgres:
     with oracle_pool.begin() as ora:
         extract_dataset(ora, postgres, """
+        SELECT DISTINCT sct.FCN_ID_TIPO_SUBCTA, sct.FCN_ID_REGIMEN, 
+               sct.FCN_ID_CAT_SUBCTA, cat.FCC_VALOR
+        FROM TCCRXGRAL_TIPO_SUBCTA sct
+        INNER JOIN THCRXGRAL_CAT_CATALOGO cat 
+                ON sct.FCN_ID_CAT_SUBCTA = cat.FCN_ID_CAT_CATALOGO
+        """, "tipo_subcuentas", 1, phase=1, limit=100)
+        extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, 
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_AVOL
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_avol", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_avol", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE, 
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, 
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_BONO
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_bono_uni", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_bono", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, 
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_COMP
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_comret", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_comret", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, 
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_GOB
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_gobierno", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_gobierno", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION,
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_RCV
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_rcv", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_rcv", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, 
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_SAR
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_sar", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_sar", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_ID_MOV, FCN_ID_TIPO_SUBCTA, FTC_FOLIO, FCN_ID_SIEFORE,
                FTF_MONTO_ACCIONES, FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION,
                FCN_ID_TIPO_MOV, FCN_ID_CONCEPTO_MOV, FTF_MONTO_PESOS
         FROM TTAFOGRAL_MOV_VIV
         WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_viv", params={"start": start_month, "end": end_month}, limit=100)
+        """, "mov_viv", 1, phase=1, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
-        SELECT DISTINCT sct.FCN_ID_TIPO_SUBCTA, sct.FCN_ID_REGIMEN, 
-               sct.FCN_ID_CAT_SUBCTA, cat.FCC_VALOR
-        FROM TCCRXGRAL_TIPO_SUBCTA sct
-        INNER JOIN THCRXGRAL_CAT_CATALOGO cat 
-                ON sct.FCN_ID_CAT_SUBCTA = cat.FCN_ID_CAT_CATALOGO
-        """, "tipo_subcuentas", limit=100)
+        SELECT FTN_ID_MOV, FTC_FOLIO, FCN_ID_SIEFORE, FTF_MONTO_ACCIONES, 
+               FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, FTF_MONTO_PESOS
+        FROM TTAFOGRAL_MOV_CMS
+        WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
+        """, "mov_comisiones", 1, phase=2, params={"start": start_month, "end": end_month}, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FCN_ID_SIEFORE,FCN_ID_REGIMEN, FCN_VALOR_ACCION, FCD_FEH_ACCION
         FROM TCAFOGRAL_VALOR_ACCION
@@ -183,25 +265,7 @@ with postgres_pool.begin() as postgres:
             SELECT MAX(FCD_FEH_ACCION) 
             FROM TCAFOGRAL_VALOR_ACCION
         )
-        """, "valor_acciones", limit=100)
-        extract_dataset(ora, postgres, """
-        SELECT FTN_ID_MOV, FTC_FOLIO, FCN_ID_SIEFORE, FTF_MONTO_ACCIONES, 
-               FTN_NUM_CTA_INVDUAL, FTD_FEH_LIQUIDACION, FTF_MONTO_PESOS
-        FROM TTAFOGRAL_MOV_CMS
-        WHERE FTD_FEH_LIQUIDACION BETWEEN :start AND :end
-        """, "mov_comisiones", params={"start": start_month, "end": end_month}, limit=100)
-        extract_dataset(ora, postgres, """
-        SELECT FTN_DIA_ACCIONES, FTC_NUM_CTA_INVDUAL, FCN_ID_SIEFORE,
-               FCN_ID_TIPO_SUBCTA, FTD_FEH_LIQUIDACION 
-        FROM THAFOGRAL_SALDO_HISTORICO
-        WHERE FTN_DIA_ACCIONES = :end_day
-        """, "saldo_inicial", params={"end_day": end_month.day}, limit=100)
-        extract_dataset(ora, postgres, """
-        SELECT FTN_DIA_ACCIONES, FTC_NUM_CTA_INVDUAL, FCN_ID_SIEFORE,
-               FCN_ID_TIPO_SUBCTA, FTD_FEH_LIQUIDACION 
-        FROM THAFOGRAL_SALDO_HISTORICO
-        WHERE FTN_DIA_ACCIONES = :start_day
-        """, "saldo_final", params={"start_day": start_month.day}, limit=100)
+        """, "valor_acciones", 1, phase=3, limit=100)
         extract_dataset(ora, postgres, """
         SELECT FTN_DIA_ACCIONES, FTC_NUM_CTA_INVDUAL, FCN_ID_SIEFORE,
                FCN_ID_TIPO_SUBCTA, FTD_FEH_LIQUIDACION
@@ -210,48 +274,51 @@ with postgres_pool.begin() as postgres:
             SELECT MAX(FTN_DIA_ACCIONES) 
             FROM THAFOGRAL_SALDO_HISTORICO
         )
-        """, "fecha_acciones", limit=100)
+        """, "fecha_acciones", 1, phase=3, limit=100)
+        extract_dataset(ora, postgres, """
+        SELECT FTN_DIA_ACCIONES, FTC_NUM_CTA_INVDUAL, FCN_ID_SIEFORE,
+               FCN_ID_TIPO_SUBCTA, FTD_FEH_LIQUIDACION 
+        FROM THAFOGRAL_SALDO_HISTORICO
+        WHERE FTN_DIA_ACCIONES = :end_day
+        """, "saldo_inicial", 1, phase=4, params={"end_day": end_month.day}, limit=100)
+        extract_dataset(ora, postgres, """
+        SELECT FTN_DIA_ACCIONES, FTC_NUM_CTA_INVDUAL, FCN_ID_SIEFORE,
+               FCN_ID_TIPO_SUBCTA, FTD_FEH_LIQUIDACION 
+        FROM THAFOGRAL_SALDO_HISTORICO
+        WHERE FTN_DIA_ACCIONES = :start_day
+        """, "saldo_final", 1, phase=5, params={"start_day": start_month.day}, limit=100)
 
+with masters_pool.begin() as masters:
     with buc_pool.begin() as buc:
-        extract_dataset(buc, postgres, """
-        SELECT C.NUMERO AS CUENTA,
-               (PF.NOMBRE ||' '||PF.APELLIDOPATERNO ||' '||PF.APELIDOMATERNO) AS NOMBRE,
-               PCR.IDPERSONA,
-               PF.IDSEXO,
+        extract_dataset(buc, masters, """
+        SELECT C.NUMERO AS CUENTA, DP.CORREOS,
+               (PF.NOMBRE || ' ' || PF.APELLIDOPATERNO || ' ' ||PF.APELIDOMATERNO) AS NOMBRE,
+               DI.CALLE, DI.NUMEROEXTERIOR AS NUMERO, ASE.NOMBRE AS COLONIA,
+               C.NOMBRE AS DELEGACION, CP.CODIGOPOSTAL, E.NOMBRE AS ENTIDAD_FEDERATIVA,
                NSS.VALOR_IDENTIFICADOR AS NSS,
-               CURP.VALOR_IDENTIFICADOR AS CURP,
-               RFC.VALOR_IDENTIFICADOR AS RFC,
-               PF.FECHANACIMIENTO
+               CURP.VALOR_IDENTIFICADOR AS CURP, 
+               RFC.VALOR_IDENTIFICADOR AS RFC
         FROM CONTRATO C
-           INNER JOIN PERSONA_CONT_ROL PCR ON (C.IDCONTRATO = PCR.IDCONTRATO)
-           INNER JOIN PERSONA_FISICA PF ON (PCR.IDPERSONA = PF.IDPERSONA)
-           LEFT JOIN IDENTIFICADOR NSS ON (PCR.IDPERSONA = NSS.IDPERSONA AND NSS.IDTIPOIDENTIFICADOR = 3 AND NSS.VALIDO = 1 AND NSS.ACTIVO = 1)
-           LEFT JOIN IDENTIFICADOR CURP ON (PCR.IDPERSONA = CURP.IDPERSONA AND CURP.IDTIPOIDENTIFICADOR = 2 AND CURP.VALIDO = 1 AND CURP.ACTIVO = 1)
-           LEFT JOIN IDENTIFICADOR RFC ON (PCR.IDPERSONA = RFC.IDPERSONA AND RFC.IDTIPOIDENTIFICADOR = 1 AND RFC.VALIDO = 1 AND RFC.ACTIVO = 1)
-           INNER JOIN CATALOGO_GENERAL E ON (C.IDSTATUSCONTRATO = E.IDCATALOGOGRAL)
-        -- WHERE PCR.IDROL = 787 /* Rol cliente */
-        --   AND C.IDLINEANEGOCIO = 763 /* Linea de negocio */
-        --   AND C.NUMERO < 7000000000 /* Solo Afiliados */
-        --   AND E.CLAVENATURAL NOT IN ('21','22','24','51','90') 
-        """, "datos_demograficos", limit=10000)
-        extract_dataset(buc, postgres, """
-        SELECT D.IDDIRECCION, D.PREFERENTE, DI.VALIDO, DI.CALLE,
-           DI.NUMEROEXTERIOR, DI.NUMEROINTERIOR , CP.CODIGOPOSTAL,
-           DI.IDASENTAMIENTO, ASE.NOMBRE AS ASENTAMIENTO, M.NOMBRE AS MUNICIPIO,
-           C.NOMBRE AS CIUDAD, E.NOMBRE AS ESTADO, P.NOMBRE AS PAIS,
-           EST.DESCRIPCION AS ESTATUS_DOM, D.FECHACREACION AS FECHACREACION_DOM,
-           D.FECHAACTUALIZACION AS FECHAACTUALIZACION_DOM
-        FROM PERSONA A
-            INNER JOIN DOMICILIO D ON (A.IDPERSONA = D.IDPERSONA)
+            INNER JOIN PERSONA_CONT_ROL PCR ON (C.IDCONTRATO = PCR.IDCONTRATO)
+            INNER JOIN PERSONA P ON (PCR.IDPERSONA = P.IDPERSONA)
+            INNER JOIN PERSONA_ORIGEN PO ON (P.IDPERSONA = PO.IDPERSONA)
+            INNER JOIN DO_PERSONA DP ON (PO.IDPERSONAORIGEN = DP.IDPERSONAORIGEN)
+            INNER JOIN PERSONA_FISICA PF ON (P.IDPERSONA = PF.IDPERSONA)
+            LEFT JOIN IDENTIFICADOR NSS ON (PF.IDPERSONA = NSS.IDPERSONA AND NSS.IDTIPOIDENTIFICADOR = 3 AND NSS.VALIDO = 1 AND NSS.ACTIVO = 1)
+            LEFT JOIN IDENTIFICADOR CURP ON (PF.IDPERSONA = CURP.IDPERSONA AND CURP.IDTIPOIDENTIFICADOR = 2 AND CURP.VALIDO = 1 AND CURP.ACTIVO = 1)
+            LEFT JOIN IDENTIFICADOR RFC ON (PF.IDPERSONA = RFC.IDPERSONA AND RFC.IDTIPOIDENTIFICADOR = 1 AND RFC.VALIDO = 1 AND RFC.ACTIVO = 1)
+            INNER JOIN DOMICILIO D ON (PF.IDPERSONA = D.IDPERSONA)
             INNER JOIN DIRECCION DI ON (D.IDDIRECCION = DI.IDDIRECCION)
-            INNER JOIN CATALOGO_GENERAL EST ON (D.IDSTATUSDOM = EST.IDCATALOGOGRAL)
             LEFT JOIN CODIGO_POSTAL CP ON (DI.IDCODIGOPOSTAL = CP.IDCODIGOPOSTAL)
-            LEFT JOIN ASENTAMIENTO ASE ON (DI.IDASENTAMIENTO = ASE.IDASENTAMIENTO)
             LEFT JOIN ESTADO E ON (CP.IDESTADO = E.IDESTADO)
-            LEFT JOIN CIUDAD C ON (CP.IDCIUDAD = C.IDCIUDAD)
             LEFT JOIN MUNICIPIO M ON (CP.IDMUNICIPIO = M.IDMUNICIPIO)
-            LEFT JOIN PAIS P ON (CP.IDPAIS = P.IDPAIS)
-        -- WHERE D.IDTIPODOM = 818 -- Tipo de domicilio Particular
+            LEFT JOIN CIUDAD C ON (CP.IDCIUDAD = C.IDCIUDAD)
+            LEFT JOIN ASENTAMIENTO ASE ON (DI.IDASENTAMIENTO = ASE.IDASENTAMIENTO)
+        -- WHERE PCR.IDROL = 787 -- Rol cliente
+        --   AND C.IDLINEANEGOCIO = 763 -- Linea de negocio
+        --   AND C.NUMERO < 7000000000 -- Solo Afiliados
+        --   AND E.CLAVENATURAL NOT IN ('21','22','24','51','90')
+        --   AND D.IDTIPODOM = 818 /Tipo de domicilio Particular/
         --   AND D.IDSTATUSDOM = 761 -- ACTIVO
-        --   AND D.PREFERENTE = 1 -- Domicilio preferente
-        """, "datos_geograficos", limit=100)
+        --   AND D.PREFERENTE = 1 --Domicilio preferente
+        """, "datos_geograficos", 1, phase=6, limit=100)
