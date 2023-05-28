d9b51e958814abb36ea980768a8bb39a0abddc0e
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2023-05-28 12:56:26 -0600

    Separación de librerías de scripts de extracción.
    - Se separaron las librerías y funcionalidades compartidas del proyecto en un módulo separado.
    - Se crearon scripts de consola para su ejecución en reproceso.

---
+++ profuturo/database.py
@@ -0,0 +1,71 @@
+from sqlalchemy import text
+import sqlalchemy
+import oracledb
+import psycopg2
+
+
+def get_mit_conn() -> oracledb.Connection:
+    return oracledb.connect(
+        host="172.22.180.190",
+        service_name="mitafore.profuturo-gnp.net",
+        user="PROFUTURO_QAMOD",
+        password="Pa55w0rd*19",
+    )
+
+
+def get_buc_conn() -> oracledb.Connection:
+    return oracledb.connect(
+        host="172.22.164.19",
+        port=16161,
+        service_name="QA34",
+        user="CLUNICO",
+        password="temp4now13",
+    )
+
+
+def get_postgres_conn():
+    return psycopg2.connect(
+        host="34.72.193.129",
+        user="alexo",
+        password="Alexo123",
+        database="postgres",
+        options="-c search_path=maestros,gestor,hechos",
+    )
+
+
+def get_mit_pool():
+    oracledb.init_oracle_client()
+
+    mit_pool = sqlalchemy.create_engine(
+        "oracle+oracledb://",
+        creator=get_mit_conn,
+    )
+    with mit_pool.connect() as conn:
+        conn.execute(text("SELECT 'OK' FROM dual"))
+        print("MIT connection OK")
+
+    return mit_pool
+
+
+def get_buc_pool():
+    buc_pool = sqlalchemy.create_engine(
+        "oracle+oracledb://",
+        creator=get_buc_conn,
+    )
+    with buc_pool.connect() as conn:
+        conn.execute(text("SELECT 'OK' FROM dual"))
+        print("BUC connection OK")
+
+    return buc_pool
+
+
+def get_postgres_pool():
+    postgres_pool = sqlalchemy.create_engine(
+        "postgresql+psycopg2://",
+        creator=get_postgres_conn,
+    )
+    with postgres_pool.connect() as conn:
+        conn.execute(text("SELECT 'OK'"))
+        print("Postgres connection OK")
+
+    return postgres_pool
---
+++ profuturo/extraction.py
@@ -0,0 +1,124 @@
+from sqlalchemy.engine import Connection
+from sqlalchemy import text
+from typing import Dict, List, Any
+from datetime import datetime, date
+from .common import write_binnacle, notify
+from ._helpers import group_by, chunk
+import calendar
+import pandas as pd
+
+
+def extract_terms(conn: Connection) -> List[Dict[str, Any]]:
+    cursor = conn.execute(text("""
+    SELECT ftn_id_periodo, ftc_periodo
+    FROM tcgespro_periodos
+    """))
+    terms = []
+
+    for row in cursor.fetchall():
+        term = row[1].split('-')
+        year = int(term[0])
+        month = int(term[1])
+
+        month_range = calendar.monthrange(year, month)
+        start_month = date(year, month, 1)
+        end_month = date(year, month, month_range[1])
+
+        terms.append({"id": row[0], "start_month": start_month, "end_month": end_month})
+        print(f"Extracting period: from {start_month} to {end_month}")
+
+    return terms
+
+
+def extract_indicator(
+    origin: Connection,
+    destination: Connection,
+    query: str,
+    index: int,
+    phase: int,
+    params: Dict[str, Any] = None,
+    limit: int = None,
+):
+    if params is None:
+        params = {}
+    if limit is not None:
+        query = f"SELECT * FROM ({query}) WHERE ROWNUM <= :limit"
+        params["limit"] = limit
+
+    print("Extracting fixed indicator...")
+
+    start = datetime.now()
+
+    cursor = origin.execute(text(query), params)
+    for value, accounts in group_by(cursor.fetchall(), lambda row: row[1], lambda row: row[0]).items():
+        for i, batch in enumerate(chunk(accounts, 1_000)):
+            destination.execute(text("""
+            UPDATE tcdatmae_clientes
+            SET FTA_INDICADORES[:index] = :value
+            WHERE FTN_CUENTA IN :accounts
+            """), {
+                "accounts": tuple(batch),
+                "index": index,
+                "value": value,
+            })
+
+            print(f"Updating records {i * 1_000} throught {(i + 1) * 1_000}")
+
+    end = datetime.now()
+
+    write_binnacle(destination, phase, start, end)
+
+    print("Done extracting fixed indicator!")
+
+
+def extract_dataset(
+    origin: Connection,
+    destination: Connection,
+    query: str,
+    table: str,
+    term: int,
+    phase: int,
+    params: Dict[str, Any] = None,
+    limit: int = None,
+    with_term: bool = True,
+):
+    if params is None:
+        params = {}
+    if limit is not None:
+        query = f"SELECT * FROM ({query}) WHERE ROWNUM <= :limit"
+        params["limit"] = limit
+
+    print(f"Extracting {table}...")
+
+    start = datetime.now()
+
+    try:
+        df_pd = pd.read_sql_query(text(query), origin, params=params)
+
+        if with_term:
+            df_pd = df_pd.assign(fcn_id_periodo=term)
+
+        df_pd.to_sql(
+            table,
+            destination,
+            if_exists="append",
+            index=False,
+            method="multi",
+            chunksize=1_000,
+        )
+    except Exception as e:
+        notify(
+            destination,
+            term,
+            f"Error al ingestar {table}",
+            f"Hubo un error al ingestar {table}",
+            f"Mensaje de error: {e}"
+        )
+        raise e
+
+    end = datetime.now()
+
+    write_binnacle(destination, phase, start, end, term=term)
+
+    print(f"Done extracting {table}!")
+    print(df_pd.info())
