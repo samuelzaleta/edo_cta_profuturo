99d37cca09c01d4548291263958f1dab38b806ab
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2023-09-03 15:33:27 -0600

    Implementación de indicadores manuales

--- bin/indicadoresDinamicos.py
+++ bin/indicadoresDinamicos.py
@@ -1,6 +1,6 @@
 from sqlalchemy import text
 from profuturo.common import register_time, define_extraction, truncate_table
-from profuturo.database import SparkConnectionConfigurator, get_postgres_pool, configure_mit_spark, configure_postgres_spark, configure_buc_spark, configure_integrity_spark, configure_bigquery_spark
+from profuturo.database import SparkConnectionConfigurator, get_postgres_pool, configure_mit_spark, configure_postgres_spark, configure_buc_spark, configure_integrity_spark
 from profuturo.extraction import update_indicator_spark, extract_dataset_spark
 from profuturo.reporters import HtmlReporter
 from profuturo.extraction import extract_terms
@@ -9,7 +9,6 @@ import sys
 html_reporter = HtmlReporter()
 postgres_pool = get_postgres_pool()
 phase = int(sys.argv[1])
-area = int(sys.argv[4])
 
 with define_extraction(phase, postgres_pool, postgres_pool) as (postgres, _):
     term = extract_terms(postgres, phase)
@@ -17,8 +16,8 @@ with define_extraction(phase, postgres_pool, postgres_pool) as (postgres, _):
     start_month = term["start_month"]
     end_month = term["end_month"]
 
-    with register_time(postgres_pool, phase,area ,term_id):
-        # Extracción
+    with register_time(postgres_pool, phase, term_id):
+        # Indicadores dinámicos
         truncate_table(postgres, "TCHECHOS_CLIENTE_INDICADOR", term=term_id)
         indicators = postgres.execute(text("""
         SELECT "FTN_ID_INDICADOR", "FTC_DESCRIPCION", "FTB_DISPONIBLE", "FTB_IMPRESION", "FTB_ENVIO", "FTB_GENERACION"
@@ -53,24 +52,10 @@ with define_extraction(phase, postgres_pool, postgres_pool) as (postgres, _):
 
             print(f"Done extracting {indicator[1]}!")
 
-        extract_dataset_spark(
-            configure_postgres_spark,
-            configure_bigquery_spark,
-            f"""
-            SELECT "FCN_CUENTA",
-                   "FCN_ID_PERIODO",
-                   ("FTN_EVALUA_INDICADOR" & 8) >> 3 AS FTB_DISPONIBLE,
-                   ("FTN_EVALUA_INDICADOR" & 4) >> 2 AS FTB_IMPRESION,
-                   ("FTN_EVALUA_INDICADOR" & 2) >> 1 AS FTB_ENVIO,
-                   "FTN_EVALUA_INDICADOR" & 1 AS FTB_GENERACION
-            FROM (
-                SELECT "FCN_CUENTA", "FCN_ID_PERIODO", bit_and("FTN_EVALUA_INDICADOR") AS "FTN_EVALUA_INDICADOR"
-                FROM "HECHOS"."TCHECHOS_CLIENTE_INDICADOR"
-                WHERE "FCN_ID_PERIODO" = :term
-                GROUP BY "FCN_CUENTA", "FCN_ID_PERIODO"
-            ) AS temp
-            """,
-            'cifras_control.TEST_INDICADORES',
-            term=term_id,
-            area=area
-        )
+        # Indicadores manuales
+        extract_dataset_spark(configure_postgres_spark, configure_postgres_spark, """
+        SELECT "FCN_CUENTA", "FTN_ID_INDICADOR" AS "FCN_ID_INDICADOR", 1 AS "FTN_EVALUA_INDICADOR"
+        FROM "HECHOS"."TTHECHOS_CARGA_ARCHIVO" CA
+            INNER JOIN "GESTOR"."TCGESPRO_INDICADOR" I ON CA."FTC_TRAMITE" = I."FTC_TRAMITE"
+        WHERE "FCN_ID_PERIODO" = :term
+        """, '"HECHOS"."TCHECHOS_CLIENTE_INDICADOR"', term=term_id, params={'term': term_id})
