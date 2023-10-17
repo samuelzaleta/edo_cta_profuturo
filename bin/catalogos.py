ec8abf6f915b48e4d22a63536ac65b9f838a06ed
Salvador Vidal <salvador.vidalm97@gmail.com>
2023-10-17 10:23:25 -0600

    Add variables filter indicadores dinamicos

--- bin/catalogos.py
+++ bin/catalogos.py
@@ -129,4 +129,6 @@ with define_extraction(phase, area, postgres_pool, mit_pool) as (postgres, mit):
             area,
             term=term_id,
             message=f"Se han ingestado los catálogos de forma exitosa para el periodo {time_period}",
+            aprobar=False,
+            descarga=False
         )
