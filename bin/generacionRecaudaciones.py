9c00f9f5e68842c4941e917f1197689271e262c8
Salvador Vidal <salvador.vidalm97@gmail.com>
2023-11-23 11:23:55 -0600

    upload to gcs

--- bin/generacionRecaudaciones.py
+++ bin/generacionRecaudaciones.py
@@ -1,15 +1,13 @@
 from profuturo.common import register_time, define_extraction, notify
 from profuturo.database import get_postgres_pool
-from profuturo.extraction import extract_terms
+from profuturo.extraction import extract_terms, _get_spark_session
 from profuturo.reporters import HtmlReporter
 from pyspark.sql import SparkSession
+from google.cloud import storage
+import pyspark.sql.functions as f
 import sys
 
-spark = SparkSession \
-    .builder \
-    .master('yarn') \
-    .appName('spark-bigquery-recaudaciones') \
-    .getOrCreate()
+spark = _get_spark_session()
 
 html_reporter = HtmlReporter()
 postgres_pool = get_postgres_pool()
@@ -17,6 +15,9 @@ postgres_pool = get_postgres_pool()
 phase = int(sys.argv[1])
 user = int(sys.argv[3])
 area = int(sys.argv[4])
+
+storage_client = storage.Client()
+bucket = storage_client.get_bucket("edo_cuenta_profuturo_dev_b")
 
 
 def extract_bigquery(table):
@@ -80,6 +81,7 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
     end_month = term["end_month"]
 
     with register_time(postgres_pool, phase, term_id, user, area):
+
         general_count = 0
         ahorro_count = 0
         bono_count = 0
@@ -95,8 +97,9 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                         "FTF_VALOR_NOMINAL_PESO"]
         saldo_columns = ["FTN_ID_CONCEPTO", "FTC_CONCEPTO_NEGOCIO", "FTF_SALDO_TOTAL"]
 
-        df_edo_general = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_GENERAL').filter('FCN_ID_PERIODO' == term_id)
+        df_edo_general = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_GENERAL').filter(f"FCN_ID_PERIODO == {term_id}")
         df_edo_anverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_ANVERSO')
+
         df_edo_reverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_REVERSO')
 
         df_general_anverso = df_edo_general.join(df_edo_anverso, 'FCN_ID_EDOCTA')
@@ -104,13 +107,46 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
         total_count = sum([general_count, ahorro_count, bono_count, saldo_count])
         data_strings = data_strings + f"5\n{general_count}|{ahorro_count}|{bono_count}|{saldo_count}|{total_count}|"
 
-        with open(f"gs://gestor-edo-cuenta/test_retiros/recaudacion_{term_id}", "w") as f:
-            f.write(data_strings)
+        with open("/tmp/recaudacion_reverso_{term_id}.txt", "w") as tmp_file:
+            tmp_file.write(data_strings)
+            blob = bucket.blob(f"test_retiros/recaudacion_reverso_{term_id}.txt")
+            blob.upload_from_filename(f"/tmp/recaudacion_reverso_{term_id}.txt")
 
-        reverso_columns = [""]
-        df_reverso = df_edo_reverso.select()
+            file_contents = blob.download_as_string()
+            file_contents = file_contents.decode("utf-8")
+            blob.upload_from_string(file_contents.encode("utf-8"))
 
-        notify(
+        reverso_columns = ["FCN_NUMERO_CUENTA", "FTN_ID_CONCEPTO", "FTC_SECCION", "FTD_FECHA_MOVIMIENTO",
+                           "FTC_DESC_CONCEPTO", "FTC_PERIODO_REFERENCIA", "FTN_DIA_COTIZADO", "FTN_SALARIO_BASE",
+                           "FTN_MONTO"]
+        df_reverso = df_edo_reverso.select(*reverso_columns)
+        reverso_data = df_reverso.rdd.flatMap(lambda row: [f"{row[column]}" for column in reverso_columns])
+
+        reverso_data = reverso_data.collect()
+        ret = df_reverso.filter(f.col("FTC_SECCION") == "RET").select(f.sum("FTC_SECCION")).collect()[0][0]
+        vol = df_reverso.filter(f.col("FTC_SECCION") == "VOL").select(f.sum("FTC_SECCION")).collect()[0][0]
+        viv = df_reverso.filter(f.col("FTC_SECCION") == "VIV").select(f.sum("FTC_SECCION")).collect()[0][0]
+        total = df_reverso.count()
+
+        reverso_data_str = "\n".join(map(lambda row: "|".join(row), reverso_data))
+
+        with open("/tmp/recaudacion_reverso_{term_id}.txt", "w") as tmp_file:
+            tmp_file.write("1\n")
+            tmp_file.write(reverso_data_str)
+            tmp_file.write("\n2\n")
+            tmp_file.write(f"{ret}|{vol}|{viv}|{total}|{total}|")
+
+        blob = bucket.blob(f"test_retiros/recaudacion_reverso_{term_id}.txt")
+        blob.upload_from_filename(f"/tmp/recaudacion_reverso_{term_id}.txt")
+
+        file_contents = blob.download_as_string()
+        additional_data = f"\n2\n{ret}|{vol}|{viv}|{total}|{total}|"
+        file_contents = file_contents.decode("utf-8") + additional_data
+        blob.upload_from_string(file_contents.encode("utf-8"))
+
+
+
+        """notify(
             postgres,
             f"Generacion de archivos Recaudaciones",
             phase,
@@ -119,4 +155,4 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
             message=f"Se han exportado recaudaciones para el periodo {time_period}",
             aprobar=False,
             descarga=False
-        )
+        )"""
