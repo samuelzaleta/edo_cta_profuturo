9cea0d05e99a7abeea7bc5dafbadc8008564bd7f
Salvador Vidal <salvador.vidalm97@gmail.com>
2023-11-24 15:35:40 -0600

    reverso data

--- bin/generacionRecaudaciones.py
+++ bin/generacionRecaudaciones.py
@@ -27,51 +27,22 @@ def extract_bigquery(table):
     return df
 
 
-def format_row(row):
-    global general_count
-    global ahorro_count
-    global bono_count
-    global saldo_count
+def get_data(index, columns, df):
+    df = df.select(*columns)
+    data = df.rdd.flatMap(lambda row: [f"{row[column]}" for column in reverso_columns]).collect()
+    data = [list(data[i:i + len(columns)]) for i in range(0, len(data), len(columns))]
+    res = f"{index}\n"
+    for row in data:
+        data_str = "|".join(row[i] for i in range(len(reverso_columns)))
+        res += data_str + "\n"
 
-    general_has_data = any(row[column] is not None for column in general_columns)
-    ahorro_has_data = any(row[column] is not None for column in ahorro_columns)
-    bono_has_data = any(row[column] is not None for column in bono_columns)
-    saldo_has_data = any(row[column] is not None for column in saldo_columns)
+    return res, len(data)
 
-    formatted_data = []
 
-    if general_has_data:
-        general_data = [f"{row[column]}" for column in general_columns]
-        formatted_data.append("1\n" + "|".join(general_data))
-        general_count += 1
-    else:
-        formatted_data.append("1\n")
+def str_to_gcs(data, name, term_id):
+    blob = bucket.blob(f"test_retiros/{name}_{term_id}.txt")
+    blob.upload_from_string(data)
 
-    if ahorro_has_data:
-        ahorro_data = [f"{row[column]}" for column in ahorro_columns]
-        formatted_data.append("2\n" + "|".join(ahorro_data))
-        ahorro_count += 1
-    else:
-        formatted_data.append("2\n")
-
-    if bono_has_data:
-        bono_data = [f"{row[column]}" for column in bono_columns]
-        formatted_data.append("3\n" + "|".join(bono_data))
-        bono_count += 1
-    else:
-        formatted_data.append("3\n")
-
-    if saldo_has_data:
-        saldo_data = [f"{row[column]}" for column in saldo_columns]
-        formatted_data.append("4\n" + "|".join(saldo_data))
-        saldo_count += 1
-    else:
-        formatted_data.append("4\n")
-
-    if formatted_data:
-        return "\n".join(formatted_data)
-    else:
-        return None
 
 with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, _):
     term = extract_terms(postgres, phase)
@@ -81,11 +52,6 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
     end_month = term["end_month"]
 
     with register_time(postgres_pool, phase, term_id, user, area):
-
-        general_count = 0
-        ahorro_count = 0
-        bono_count = 0
-        saldo_count = 0
 
         general_columns = ["FCN_FOLIO", "FTC_NOMBRE_COMPLETO", "FTC_CALLE_NUMERO", "FTC_COLONIA", "FTC_DELEGACION",
                            "FTN_CP", "FTC_ENTIDAD_FEDERATIVA", "FTC_RFC", "FTC_NSS", "FTC_CURP",
@@ -103,45 +69,31 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
         df_edo_reverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_REVERSO')
 
         df_general_anverso = df_edo_general.join(df_edo_anverso, 'FCN_ID_EDOCTA')
-        data_strings = df_general_anverso.rdd.map(format_row).collect()
+        data_general, general_count = get_data(1, general_columns, df_general_anverso)
+        data_ahorro, ahorro_count = get_data(2, ahorro_columns, df_general_anverso)
+        data_bono, bono_count= get_data(3, bono_columns, df_general_anverso)
+        data_saldo, saldo_count = get_data(4, saldo_columns, df_general_anverso)
+
         total_count = sum([general_count, ahorro_count, bono_count, saldo_count])
-        data_strings = data_strings + f"5\n{general_count}|{ahorro_count}|{bono_count}|{saldo_count}|{total_count}|"
+        final_row = f"5\n{general_count}|{ahorro_count}|{bono_count}|{saldo_count}|{total_count}|"
+        data_strings = data_general + data_ahorro, data_bono, data_saldo + final_row
 
-        with open("/tmp/recaudacion_reverso_{term_id}.txt", "w") as tmp_file:
-            tmp_file.write(data_strings)
-            blob = bucket.blob(f"test_retiros/recaudacion_anverso_{term_id}.txt")
-            blob.upload_from_filename(f"/tmp/recaudacion_anverso_{term_id}.txt")
-
-            file_contents = blob.download_as_string()
-            file_contents = file_contents.decode("utf-8")
-            blob.upload_from_string(file_contents.encode("utf-8"))
+        str_to_gcs(data_strings, "recaudacion_reverso", term_id)
 
         reverso_columns = ["FCN_NUMERO_CUENTA", "FTN_ID_CONCEPTO", "FTC_SECCION", "FTD_FECHA_MOVIMIENTO",
                            "FTC_DESC_CONCEPTO", "FTC_PERIODO_REFERENCIA", "FTN_DIA_COTIZADO", "FTN_SALARIO_BASE",
                            "FTN_MONTO"]
-        df_reverso = df_edo_reverso.select(*reverso_columns)
-        reverso_data = df_reverso.rdd.flatMap(lambda row: [f"{row[column]}" for column in reverso_columns])
-        reverso_data = reverso_data.collect()
-        ret = df_reverso.filter(f.col("FTC_SECCION") == "RET").select(f.sum("FTC_SECCION")).collect()[0][0]
-        vol = df_reverso.filter(f.col("FTC_SECCION") == "VOL").select(f.sum("FTC_SECCION")).collect()[0][0]
-        viv = df_reverso.filter(f.col("FTC_SECCION") == "VIV").select(f.sum("FTC_SECCION")).collect()
-        total = df_reverso.count()
-        reverso_data = [list(reverso_data[i:i + len(reverso_columns)]) for i in
-                        range(0, len(reverso_data), len(reverso_columns))]
-        reverso_data_str = "1\n"
-
-        for row in reverso_data:
-            reverso_str = "|".join(row[i] for i in range(len(reverso_columns)))
-            reverso_data_str += reverso_str + "\n"
-
-        blob = bucket.blob(f"test_retiros/recaudacion_reverso_{term_id}.txt")
-        blob.upload_from_filename(f"/tmp/recaudacion_reverso_{term_id}.txt")
-        reverso_data_str = reverso_data_str + f"2\n{ret}|{vol}|{viv}|{total}|"
-        blob.upload_from_string(reverso_data_str)
+        reverso_data, reverso_count = get_data(1, reverso_columns, df_edo_reverso)
+        ret = df_edo_reverso.filter(f.col("FTC_SECCION") == "RET").select(f.sum("FTC_SECCION")).collect()[0][0]
+        vol = df_edo_reverso.filter(f.col("FTC_SECCION") == "VOL").select(f.sum("FTC_SECCION")).collect()[0][0]
+        viv = df_edo_reverso.filter(f.col("FTC_SECCION") == "VIV").select(f.sum("FTC_SECCION")).collect()
+        total = df_edo_reverso.count()
+        reverso_final_row = f"2\n{ret}|{vol}|{viv}|{total}|"
+        final_reverso = reverso_data + reverso_final_row
+        str_to_gcs(final_reverso, "recaudacion_reverso", term_id)
 
 
-
-        """notify(
+        notify(
             postgres,
             f"Generacion de archivos Recaudaciones",
             phase,
@@ -150,4 +102,4 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
             message=f"Se han exportado recaudaciones para el periodo {time_period}",
             aprobar=False,
             descarga=False
-        )"""
+        )
