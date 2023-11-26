110b37d56e94b4eb2afa72511760fe6f8edf1fe4
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2023-11-26 11:45:16 -0600

    Mejora de extracción

--- profuturo/extraction.py
+++ profuturo/extraction.py
@@ -390,7 +390,7 @@ def _get_spark_session() -> SparkSession:
 
 
 def _create_spark_dataframe(spark: SparkSession, connection_configurator, query: str, params: Dict[str, Any]) -> SparkDataFrame:
-    return connection_configurator(spark.read, f"({_replace_query_params(query, params)}) dataset", True) \
+    return connection_configurator(spark.read, _replace_query_params(query, params), True) \
         .load() \
         .cache()
 
