dd848f830bf318da6f48b3d3fbf8b9c487a44a29
samuelzaleta <samuel.zaleta.cbtis103@gmail.com>
2023-12-04 13:56:32 -0600

    retiros version 20 implementacion todo en una sola tabla, con vivienda y retiro en un solo registro

--- bin/retiros.py
+++ bin/retiros.py
@@ -33,9 +33,9 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
     spark = _get_spark_session()
 
     with register_time(postgres_pool, phase, term_id, user, area):
-        #truncate_table(postgres, 'TTHECHOS_RETIRO_TEST', term=term_id)
-        truncate_table(postgres, 'TTHECHOS_RETIRO_LIQUIDACIONES', term=term_id)
-        truncate_table(postgres, 'TTHECHOS_RETIRO_SALDOS_INICIALES', term=term_id)
+        truncate_table(postgres, 'TTHECHOS_RETIRO', term=term_id)
+        #truncate_table(postgres, 'TTHECHOS_RETIRO_LIQUIDACIONES', term=term_id)
+        #truncate_table(postgres, 'TTHECHOS_RETIRO_SALDOS_INICIALES', term=term_id)
 
         query_retiros = """
         WITH LIQ_SOLICITUDES AS (
@@ -258,14 +258,11 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                  ON PS.FTC_FOLIO = RET.FTC_FOLIO
         WHERE (PS.FTC_FOLIO, PS.FTN_NUM_REEXP) IN (
                             SELECT PSMAX.FTC_FOLIO,MAX(PSMAX.FTN_NUM_REEXP)FROM BENEFICIOS.TTCRXGRAL_PAGO_SUBCTA PSMAX
-                                INNER JOIN LIQ_SOLICITUDES LS
-                                ON PSMAX.FTC_FOLIO = LS.FTC_FOLIO
                             GROUP BY PSMAX.FTC_FOLIO)
         --AND RET.FTN_NUM_CTA_INVDUAL = 3200559346
-        GROUP BY PS.FTC_FOLIO, RET.FTN_NUM_CTA_INVDUAL,RET.FTC_FOLIO_REL
-        """
+        GROUP BY PS.FTC_FOLIO, RET.FTN_NUM_CTA_INVDUAL,RET.FTC_FOLIO_REL """
 
-        query_saldos = """
+        query_saldos ="""
         WITH RETIROS AS (
             SELECT X.FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, X.FTC_FOLIO, X.FTC_FOLIO_REL,
                    PT.TMC_DESC_ITGY AS FTC_TMC_DESC_ITGY, PT.TMC_DESC_NCI, PT.TMN_CVE_NCI,
@@ -298,7 +295,9 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                 )
             ) PT ON PT.FCN_ID_SUBPROCESO = X.FCN_ID_SUBPROCESO
         ), SALDOS_AL_DIA_LIQUIDACION AS (
-            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, SUM(FTF_SALDO_DIA) AS FTF_SALDO_DIA
+
+            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, FTD_FEH_CRE,
+                   SUM(FTF_SALDO_DIA) AS FTF_SALDO_DIA
             FROM (
                 SELECT DISTINCT SH.FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, SH.FTD_FEH_LIQUIDACION,
                        RET.FTD_FEH_CRE, SH.FCN_ID_SIEFORE, SH.FCN_ID_TIPO_SUBCTA, RET.FTC_TMC_DESC_ITGY,
@@ -310,40 +309,50 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                     INNER JOIN RETIROS RET ON SH.FTN_NUM_CTA_INVDUAL = RET.FCN_CUENTA
                     INNER JOIN cierren.TCCRXGRAL_TIPO_SUBCTA R ON R.FCN_ID_TIPO_SUBCTA = SH.FCN_ID_TIPO_SUBCTA
                     INNER JOIN (
-                        SELECT DISTINCT SH.FTN_NUM_CTA_INVDUAL, SH.FCN_ID_TIPO_SUBCTA,
-                               SH.FCN_ID_SIEFORE, MAX(TRUNC(SH.FTD_FEH_LIQUIDACION)) AS FTD_FEH_LIQUIDACION
+                        SELECT DISTINCT SH.FTN_NUM_CTA_INVDUAL, SH.FCN_ID_TIPO_SUBCTA, FTD_FEH_CRE,
+                               RET.FTC_TMC_DESC_ITGY,SH.FCN_ID_SIEFORE,
+                               MAX(TRUNC(SH.FTD_FEH_LIQUIDACION)) AS FTD_FEH_LIQUIDACION
                         FROM CIERREN.THAFOGRAL_SALDO_HISTORICO_V2 SH
                             INNER JOIN RETIROS RET ON SH.FTN_NUM_CTA_INVDUAL = RET.FCN_CUENTA
-                        WHERE SH.FTD_FEH_LIQUIDACION <= RET.FTD_FEH_CRE
+                        WHERE SH.FTD_FEH_LIQUIDACION < RET.FTD_FEH_CRE
                           AND SH.FCN_ID_TIPO_SUBCTA NOT IN (15, 16)
-                        GROUP BY SH.FTN_NUM_CTA_INVDUAL, SH.FCN_ID_TIPO_SUBCTA, SH.FCN_ID_SIEFORE
-                    ) SHMAXIMO ON SH.FTN_NUM_CTA_INVDUAL = SHMAXIMO.FTN_NUM_CTA_INVDUAL
+                        GROUP BY SH.FTN_NUM_CTA_INVDUAL, SH.FCN_ID_TIPO_SUBCTA, SH.FCN_ID_SIEFORE,
+                                 RET.FTC_TMC_DESC_ITGY,
+                                 FTD_FEH_CRE
+                        ) SHMAXIMO ON SH.FTN_NUM_CTA_INVDUAL = SHMAXIMO.FTN_NUM_CTA_INVDUAL
                               AND SH.FCN_ID_TIPO_SUBCTA = SHMAXIMO.FCN_ID_TIPO_SUBCTA
                               AND SH.FCN_ID_SIEFORE = SHMAXIMO.FCN_ID_SIEFORE
                               AND SH.FTD_FEH_LIQUIDACION = SHMAXIMO.FTD_FEH_LIQUIDACION
+                              AND RET.FTD_FEH_CRE = SHMAXIMO.FTD_FEH_CRE
+                              AND RET.FTC_TMC_DESC_ITGY = SHMAXIMO.FTC_TMC_DESC_ITGY
                     INNER JOIN cierren.TCAFOGRAL_VALOR_ACCION VA
                         ON VA.FCD_FEH_ACCION = TO_DATE(TO_CHAR(RET.FTD_FEH_CRE, 'dd/MM/yyyy'), 'dd/MM/yyyy')
                        AND SH.FCN_ID_SIEFORE = VA.FCN_ID_SIEFORE AND R.FCN_ID_REGIMEN = VA.FCN_ID_REGIMEN
             ) X
-            GROUP BY FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY
+            GROUP BY FCN_CUENTA,FTN_TIPO_AHORRO,
+                     FTC_TMC_DESC_ITGY, FTD_FEH_CRE
         ), SALDOS_INICIO_MES AS (
-            SELECT SH.FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, 1 AS FTN_TIPO_AHORRO, RET.FTC_TMC_DESC_ITGY,
+            SELECT SH.FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, 1 AS FTN_TIPO_AHORRO, RET.FTC_TMC_DESC_ITGY, RET.FTD_FEH_CRE,
                    SUM(ROUND(SH.FTN_DIA_ACCIONES * VA.FCN_VALOR_ACCION, 2)) AS FTF_SALDO_DIA
             FROM cierren.thafogral_saldo_historico_v2 SH
                 INNER JOIN cierren.TCCRXGRAL_TIPO_SUBCTA R ON R.FCN_ID_TIPO_SUBCTA = SH.FCN_ID_TIPO_SUBCTA
                 INNER JOIN RETIROS RET ON RET.FCN_CUENTA = SH.FTN_NUM_CTA_INVDUAL
                 INNER JOIN (
                     SELECT SHMAX.FTN_NUM_CTA_INVDUAL, SHMAX.FCN_ID_SIEFORE, SHMAX.FCN_ID_TIPO_SUBCTA,
+                           RET.FTC_TMC_DESC_ITGY, RET.FTD_FEH_CRE,
                            MAX(TRUNC(SHMAX.FTD_FEH_LIQUIDACION)) AS FTD_FEH_LIQUIDACION
                     FROM cierren.thafogral_saldo_historico_v2 SHMAX
                         INNER JOIN RETIROS RET ON RET.FCN_CUENTA = SHMAX.FTN_NUM_CTA_INVDUAL
                     WHERE SHMAX.FTD_FEH_LIQUIDACION <= DATE '2023-03-01' -- :start
                       AND SHMAX.FCN_ID_TIPO_SUBCTA IN (15, 16)
-                    GROUP BY SHMAX.FTN_NUM_CTA_INVDUAL, SHMAX.FCN_ID_SIEFORE, SHMAX.FCN_ID_TIPO_SUBCTA
+                    GROUP BY SHMAX.FTN_NUM_CTA_INVDUAL, SHMAX.FCN_ID_SIEFORE, SHMAX.FCN_ID_TIPO_SUBCTA,
+                           RET.FTC_TMC_DESC_ITGY, RET.FTD_FEH_CRE
                 ) SHMAXIMO ON SH.FTN_NUM_CTA_INVDUAL = SHMAXIMO.FTN_NUM_CTA_INVDUAL
                           AND SH.FCN_ID_TIPO_SUBCTA = SHMAXIMO.FCN_ID_TIPO_SUBCTA
                           AND SH.FCN_ID_SIEFORE = SHMAXIMO.FCN_ID_SIEFORE
                           AND SH.FTD_FEH_LIQUIDACION = SHMAXIMO.FTD_FEH_LIQUIDACION
+                          AND RET.FTD_FEH_CRE = SHMAXIMO.FTD_FEH_CRE
+                          AND RET.FTC_TMC_DESC_ITGY = SHMAXIMO.FTC_TMC_DESC_ITGY
                 INNER JOIN (
                     SELECT ROW_NUMBER() OVER(PARTITION BY FCN_ID_SIEFORE, FCN_ID_REGIMEN ORDER BY FCD_FEH_ACCION DESC) AS ROW_NUM,
                            FCN_ID_SIEFORE, FCN_ID_REGIMEN, FCN_VALOR_ACCION, FCD_FEH_ACCION
@@ -352,43 +361,43 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                 ) VA ON SH.FCN_ID_SIEFORE = VA.FCN_ID_SIEFORE
                     AND R.FCN_ID_REGIMEN = VA.FCN_ID_REGIMEN
                     AND VA.ROW_NUM = 1
-            GROUP BY SH.FTN_NUM_CTA_INVDUAL, RET.FTC_TMC_DESC_ITGY
+            GROUP BY SH.FTN_NUM_CTA_INVDUAL, RET.FTC_TMC_DESC_ITGY, RET.FTD_FEH_CRE
         ), SALDOS_CHEQUERA AS (
-            SELECT FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, FTC_TMC_DESC_ITGY, FTN_TIPO_AHORRO,
+            SELECT FTN_NUM_CTA_INVDUAL AS FCN_CUENTA, FTC_TMC_DESC_ITGY, FTN_TIPO_AHORRO, FTD_FEH_CRE,
                    SUM(PESOS) AS FTF_SALDO_DIA
             FROM (
                 SELECT FTN_NUM_CTA_INVDUAL, FCN_ID_TIPO_SUBCTA, r.FTC_TMC_DESC_ITGY,
                        CASE WHEN FCN_ID_TIPO_SUBCTA IN (15, 16, 17, 18) THEN 1 ELSE 0 END FTN_TIPO_AHORRO,
-                       FCN_ID_SIEFORE, SUM(FTN_DIA_PESOS) PESOS
+                       FCN_ID_SIEFORE, r.FTD_FEH_CRE,SUM(FTN_DIA_PESOS) PESOS
                 FROM TTAFOGRAL_BALANCE_MOVS_CHEQ q
                     INNER JOIN RETIROS r ON q.FTN_NUM_CTA_INVDUAL = r.FCN_CUENTA
                 WHERE FTD_FEH_LIQUIDACION < DATE '2023-03-01' -- :start
                   AND q.FCN_ID_SUBPROCESO IN (307, 324, 314, 341, 6827, 7301, 9542)
                   AND R.FTC_TMC_DESC_ITGY IN ('TJU', 'TGF', 'TPG', 'TRJ', 'TRU', 'TIV')
-                GROUP BY FTN_NUM_CTA_INVDUAL, FCN_ID_TIPO_SUBCTA, FCN_ID_SIEFORE, r.FTC_TMC_DESC_ITGY
+                GROUP BY FTN_NUM_CTA_INVDUAL, FCN_ID_TIPO_SUBCTA, FCN_ID_SIEFORE, r.FTC_TMC_DESC_ITGY, r.FTD_FEH_CRE
                 HAVING SUM(FTN_DIA_PESOS) > 0
             ) X
-            GROUP BY FTN_NUM_CTA_INVDUAL, FTC_TMC_DESC_ITGY, FTN_TIPO_AHORRO
+            GROUP BY FTN_NUM_CTA_INVDUAL, FTC_TMC_DESC_ITGY, FTN_TIPO_AHORRO, FTD_FEH_CRE
         )
-        SELECT FCN_CUENTA, FTC_TMC_DESC_ITGY,
+        SELECT FCN_CUENTA, FTC_TMC_DESC_ITGY, FTD_FEH_CRE,
         SUM(CASE WHEN FTN_TIPO_AHORRO = 1 THEN FTF_SALDO_DIA ELSE 0 END) AS FTN_SALDO_INI_VIVIENDA,
         SUM(CASE WHEN FTN_TIPO_AHORRO = 0 THEN FTF_SALDO_DIA ELSE 0 END) AS FTN_SALDO_INI_AHORRORET
         FROM (
-            SELECT FCN_CUENTA, FTN_TIPO_AHORRO,  FTC_TMC_DESC_ITGY,
+            SELECT FCN_CUENTA, FTN_TIPO_AHORRO,  FTC_TMC_DESC_ITGY, FTD_FEH_CRE,
             FTF_SALDO_DIA
             FROM SALDOS_AL_DIA_LIQUIDACION
 
             UNION ALL
 
-            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, FTF_SALDO_DIA
+            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, FTD_FEH_CRE, FTF_SALDO_DIA
             FROM SALDOS_INICIO_MES
 
             UNION ALL
 
-            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, FTF_SALDO_DIA
+            SELECT FCN_CUENTA, FTN_TIPO_AHORRO, FTC_TMC_DESC_ITGY, FTD_FEH_CRE, FTF_SALDO_DIA
             FROM SALDOS_CHEQUERA
         ) X
-        GROUP BY FCN_CUENTA, FTC_TMC_DESC_ITGY
+        GROUP BY FCN_CUENTA, FTC_TMC_DESC_ITGY, FTD_FEH_CRE
         """
 
         read_table_insert_temp_view(
@@ -451,21 +460,47 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
         """)
 
         df.show()
-        """
-        FTN_NUM_CTA_INVDUAL
-        """
 
         df = df.withColumn("FCN_ID_PERIODO", lit(term_id))
 
-        _write_spark_dataframe(df, configure_postgres_spark, '"HECHOS"."TTHECHOS_RETIRO_LIQUIDACIONES"')
+        df.createOrReplaceTempView("INFORMACIONRETIROS")
+
+        #_write_spark_dataframe(df, configure_postgres_spark, '"HECHOS"."TTHECHOS_RETIRO_LIQUIDACIONES"')
 
         df = spark.sql("""
-        SELECT * FROM SALDOS_INICIALES
+        SELECT 
+        DISTINCT
+        RL.FCN_CUENTA,
+        RL.FTC_FOLIO,
+        SL.FTN_SALDO_INI_AHORRORET as FTN_SDO_INI_AHORRORET,
+        SL.FTN_SALDO_INI_VIVIENDA as FTN_SDO_INI_VIVIENDA,
+        RL.FTN_SALDO_TRANSFERIDO_AHORRORET as FTN_SDO_TRA_AHORRORET,
+        RL.FTN_SALDO_TRANSFERIDO_VIVIENDA as FTN_SDO_TRA_VIVIENDA,
+        SL.FTN_SALDO_INI_AHORRORET - RL.FTN_SALDO_TRANSFERIDO_AHORRORET AS FTN_SDO_REM_AHORRORET,
+        SL.FTN_SALDO_INI_VIVIENDA - RL.FTN_SALDO_TRANSFERIDO_VIVIENDA AS FTN_SDO_REM_VIVIENDA,
+        RL.FTC_LEY_PENSION,
+        RL.FTC_REGIMEN,
+        RL.FTC_TPSEGURO,
+        RL.FTC_TPPENSION,
+        RL.FTC_FON_ENTIDAD,
+        RL.FTD_FEH_CRE AS FTD_FECHA_EMISION,
+        --0 AS FTN_RECURSO_RETENCION_ISR,
+        RL.FCC_TIPO_BANCO AS FTC_ENT_REC_TRAN,
+        RL.FCC_MEDIO_PAGO,
+        --RL.FTN_SALDO_TRANSFERIDO_VIVIENDA + RL.FTN_SALDO_TRANSFERIDO_AHORRORET AS FTN_MONTO_TRANSFERIDO_AFORE,
+        RL.FTN_ISR_LIQ_RET AS FTN_AFO_ISR,
+        RL.FTN_FEH_INI_PEN,
+        RL.FTN_FEH_RES_PEN,
+        RL.FTC_TMC_DESC_ITGY AS FTC_TIPO_TRAMITE,
+        RL.FTN_ARCHIVO,
+        RL.FCN_ID_PERIODO
+        FROM SALDOS_INICIALES SL
+        INNER JOIN INFORMACIONRETIROS RL
+         ON RL.FCN_CUENTA = SL.FCN_CUENTA  AND RL.FTC_TMC_DESC_ITGY = SL.FTC_TMC_DESC_ITGY
+        AND SL.FTD_FEH_CRE = RL.FTD_FEH_CRE
         """)
 
-        df = df.withColumn("FCN_ID_PERIODO", lit(term_id))
-
-        _write_spark_dataframe(df, configure_postgres_spark, '"HECHOS"."TTHECHOS_RETIRO_SALDOS_INICIALES"')
+        _write_spark_dataframe(df, configure_postgres_spark, '"HECHOS"."TTHECHOS_RETIRO"')
 
         # Convert PySpark DataFrame to pandas DataFrame
         #pandas_df = df.toPandas()
