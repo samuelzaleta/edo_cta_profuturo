9946246a9b23248f451cba9a643ffa1f74e0876d
Brandon Antonio Lorenzo <brandolin_a_@outlook.com>
2023-12-17 21:41:59 -0600

    Ajuste de bono
    - Se evitó duplicaciones a través de una cláusula DISTINC.

--- bin/bono.py
+++ bin/bono.py
@@ -80,7 +80,7 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
         )
 
         df = spark.sql("""
-            SELECT 
+            SELECT DISTINCT
             S.FCN_CUENTA,
             ROUND(S.FTF_DIA_ACCIONES,6) AS FTF_BON_NOM_ACC, 
             ROUND(S.FTF_SALDO_DIA,2) AS FTF_BON_NOM_PES,
