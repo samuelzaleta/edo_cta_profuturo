033a3a6f89a61c3a76d2bd46a3274ad4e4378343
Salvador Vidal <salvador.vidalm97@gmail.com>
2023-12-25 08:40:08 -0600

    Change Recaudaciones

--- bin/generacionRecaudaciones.py
+++ bin/generacionRecaudaciones.py
@@ -39,9 +39,7 @@ def extract_bigquery(table):
     return df
 
 
-def process_dataframe(df, identifier, client):
-    if "FCN_ID_EDOCTA" in df.columns:
-        df = df.filter(f.col("FCN_ID_EDOCTA") == client).drop("FCN_ID_EDOCTA")
+def process_dataframe(df, identifier):
     return df.rdd.flatMap(lambda row: [([identifier] + [row[i] for i in range(len(row))])]).collect()
 
 
@@ -62,27 +60,27 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
             extract_bigquery('ESTADO_CUENTA.TTEDOCTA_CLIENTE_INDICADOR')
             .filter((f.col("FTB_IMPRESION") == True) & (f.col("FTB_ENVIO") == False))
         )
-        df_edo_general = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_GENERAL').filter(f"FCN_ID_PERIODO == {term_id}")
-        df_edo_general = df_edo_general.join(df_indicador, df_edo_general.FCN_ID_EDOCTA == df_indicador.FCN_CUENTA,
+        df_general = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_GENERAL').filter(f"FCN_ID_PERIODO == {term_id}")
+        df_general = df_general.join(df_indicador, df_general.FCN_ID_EDOCTA == df_indicador.FCN_CUENTA,
                                              'left')
 
         df_anverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_ANVERSO')
 
-        df_anverso_general = df_anverso.join(df_edo_general, "FCN_ID_EDOCTA").fillna(value="")
+        df_anverso_general = df_anverso.join(df_general, "FCN_ID_EDOCTA").fillna(value="")
 
-        df_edo_general = (
-            df_edo_general.withColumn("FTD_FECHA_GRAL_INICIO", f.to_date(f.col("FTD_FECHA_GRAL_INICIO"), "ddMMyyyy"))
+        df_general = (
+            df_general.withColumn("FTD_FECHA_GRAL_INICIO", f.to_date(f.col("FTD_FECHA_GRAL_INICIO"), "ddMMyyyy"))
             .withColumn("FTD_FECHA_GRAL_FIN", f.to_date(f.col("FTD_FECHA_GRAL_FIN"), "ddMMyyyy"))
             .withColumn("FTD_FECHA_CORTE", f.to_date(f.col("FTD_FECHA_CORTE"), "ddMMyyyy"))
         )
 
         unique_clients = df_anverso_general.select("FCN_ID_EDOCTA").distinct().rdd.collect()
         clients = [row.FCN_ID_EDOCTA for row in unique_clients]
-        df_edo_general = df_edo_general.withColumn("vacio", f.lit(None))
+        df_general = df_general.withColumn("vacio", f.lit(None))
 
-        df_edo_general = (
-            df_edo_general.filter(f.col("FCN_ID_EDOCTA").isin(clients))
-            .select("FCN_ID_EDOCTA", "FCN_NUMERO_CUENTA", "FCN_FOLIO", "FTC_NOMBRE_COMPLETO", "FTC_CALLE_NUMERO",
+        df_general = (
+            df_general.filter(f.col("FCN_ID_EDOCTA").isin(clients))
+            .select("FCN_NUMERO_CUENTA", "FCN_FOLIO", "FTC_NOMBRE_COMPLETO", "FTC_CALLE_NUMERO",
                     "FTC_COLONIA", "FTC_DELEGACION", "FTN_CP", "FTC_ENTIDAD_FEDERATIVA", "FTC_RFC",
                     "FTC_NSS", "FTC_CURP",
                     f.date_format("FTD_FECHA_GRAL_INICIO", "ddMMyyyy").alias("FTD_FECHA_GRAL_INICIO"),
@@ -96,10 +94,11 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                     )
         )
 
+
         df_anverso_aho = (
             df_anverso_general.filter(
                 (f.col("FTC_SECCION").isin(["AHO", "PEN"])) & (f.col("FCN_ID_EDOCTA").isin(clients)))
-            .select("FCN_ID_EDOCTA", "FTC_CONCEPTO_NEGOCIO", f.col("FTN_SALDO_ANTERIOR").cast("decimal(16, 2)"),
+            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO", f.col("FTN_SALDO_ANTERIOR").cast("decimal(16, 2)"),
                     f.col("FTF_APORTACION").cast("decimal(16, 2)"),
                     f.col("FTN_RETIRO").cast("decimal(16, 2)"),
                     f.col("FTN_RENDIMIENTO").cast("decimal(16, 2)"),
@@ -110,7 +109,7 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
 
         df_anverso_bon = (
             df_anverso_general.filter((f.col("FTC_SECCION") == "BON") & (f.col("FCN_ID_EDOCTA").isin(clients)))
-            .select("FCN_ID_EDOCTA", "FTC_CONCEPTO_NEGOCIO", f.col("FTN_VALOR_ACTUAL_UDI").cast("decimal(16, 2)"),
+            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO", f.col("FTN_VALOR_ACTUAL_UDI").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_NOMINAL_UDI").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_ACTUAL_PESO").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_NOMINAL_PESO").cast("decimal(16, 2)"))
@@ -119,22 +118,21 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
 
         df_anverso_sdo = (
             df_anverso_general.filter((f.col("FTC_SECCION") == "SDO") & (f.col("FCN_ID_EDOCTA").isin(clients)))
-            .select("FCN_ID_EDOCTA", "FTC_GRUPO_CONCEPTO", "FTC_CONCEPTO_NEGOCIO",
+            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_GRUPO_CONCEPTO", "FTC_CONCEPTO_NEGOCIO",
                     f.col("FTN_SALDO_FINAL").cast("decimal(16, 2)"))
 
         )
 
-        general_count = df_edo_general.count()
+        general_count = df_general.count()
         ahorro_count = df_anverso_aho.count()
         bono_count = df_anverso_bon.count()
         saldo_count = df_anverso_sdo.count()
 
         processed_data = []
-        for client in clients:
-            processed_data += process_dataframe(df_edo_general, 1, client)
-            processed_data += process_dataframe(df_anverso_aho, 2, client)
-            processed_data += process_dataframe(df_anverso_bon, 3, client)
-            processed_data += process_dataframe(df_anverso_sdo, 4, client)
+        processed_data += process_dataframe(df_general, 1)
+        processed_data += process_dataframe(df_anverso_aho, 2)
+        processed_data += process_dataframe(df_anverso_bon, 3)
+        processed_data += process_dataframe(df_anverso_sdo, 4)
 
         res = "\n".join("|".join(str(item) for item in row) for row in processed_data)
 
@@ -147,7 +145,7 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
 
         df_edo_reverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_REVERSO')
 
-        df_reverso_general = df_edo_reverso.join(df_edo_general, "FCN_NUMERO_CUENTA")
+        df_reverso_general = df_edo_reverso.join(df_general, "FCN_NUMERO_CUENTA")
         df_reverso_general = (
             df_reverso_general.select("FCN_NUMERO_CUENTA", "FTN_ID_CONCEPTO", "FTC_SECCION",
                                       f.date_format("FTD_FECHA_MOVIMIENTO", "ddMMyyyy").alias("FTD_FECHA_MOVIMIENTO"),
@@ -156,7 +154,7 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                                       f.col("FTN_MONTO").cast("decimal(16, 2)"))
         )
 
-        reverso_data = [process_dataframe(df_reverso_general, 1, client)]
+        reverso_data = process_dataframe(df_reverso_general, 1)
 
         res = "\n".join("|".join(str(item) for item in row) for row in reverso_data)
 
