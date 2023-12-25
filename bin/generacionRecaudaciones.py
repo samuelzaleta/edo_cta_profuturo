4bc813c9b0cf09740c7843050366949f7acb4862
Salvador Vidal <salvador.vidalm97@gmail.com>
2023-12-25 08:52:17 -0600

    Change Recaudaciones

--- bin/generacionRecaudaciones.py
+++ bin/generacionRecaudaciones.py
@@ -62,9 +62,10 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
         )
         df_general = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_GENERAL').filter(f"FCN_ID_PERIODO == {term_id}")
         df_general = df_general.join(df_indicador, df_general.FCN_ID_EDOCTA == df_indicador.FCN_CUENTA,
-                                             'left')
+                                     'left')
 
-        df_anverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_ANVERSO').withColumnRenamed("FTC_NUMERO_CUENTA_ANVERSO", "FTC_NUMERO_CUENTA")
+        df_anverso = extract_bigquery('ESTADO_CUENTA.TTEDOCTA_ANVERSO').withColumnRenamed("FTC_NUMERO_CUENTA_ANVERSO",
+                                                                                          "FTC_NUMERO_CUENTA")
 
         df_anverso_general = df_anverso.join(df_general, "FCN_ID_EDOCTA").fillna(value="")
 
@@ -94,11 +95,11 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
                     )
         )
 
-
         df_anverso_aho = (
             df_anverso_general.filter(
                 (f.col("FTC_SECCION").isin(["AHO", "PEN"])) & (f.col("FCN_ID_EDOCTA").isin(clients)))
-            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO", f.col("FTN_SALDO_ANTERIOR").cast("decimal(16, 2)"),
+            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO",
+                    f.col("FTN_SALDO_ANTERIOR").cast("decimal(16, 2)"),
                     f.col("FTF_APORTACION").cast("decimal(16, 2)"),
                     f.col("FTN_RETIRO").cast("decimal(16, 2)"),
                     f.col("FTN_RENDIMIENTO").cast("decimal(16, 2)"),
@@ -109,7 +110,8 @@ with define_extraction(phase, area, postgres_pool, postgres_pool) as (postgres, 
 
         df_anverso_bon = (
             df_anverso_general.filter((f.col("FTC_SECCION") == "BON") & (f.col("FCN_ID_EDOCTA").isin(clients)))
-            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO", f.col("FTN_VALOR_ACTUAL_UDI").cast("decimal(16, 2)"),
+            .select(df_anverso_general.FCN_NUMERO_CUENTA, "FTC_CONCEPTO_NEGOCIO",
+                    f.col("FTN_VALOR_ACTUAL_UDI").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_NOMINAL_UDI").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_ACTUAL_PESO").cast("decimal(16, 2)"),
                     f.col("FTN_VALOR_NOMINAL_PESO").cast("decimal(16, 2)"))
